b922231edddb8e53b8c42d5853e96670806c92ef
2heeesss <hohooodo@gmail.com>
2021-11-21 22:24:16 +0900

    Test: add removeTodo function test

--- Assignment/ArrayHOF.test.js
+++ Assignment/ArrayHOF.test.js
@@ -166,12 +166,25 @@ describe('프로퍼티 정렬', () => {
 });
 
 describe('새로운 요소 추가', () => {
-  test(',,,', () => {
+  test('새로운 요소가 추가되면 배열의 가장 첫 요소로 들어간다.', () => {
     expect(addTodo(todos, newObj)).toEqual(
       [
         { id: 4, content: 'Test', completed: false },
         { id: 3, content: 'HTML', completed: false },
         { id: 2, content: 'CSS', completed: true },
+        { id: 1, content: 'Javascript', completed: false }
+      ]
+    );
+  });
+});
+
+const removeTodo = (todos, id) => todos.filter(val => val.id !== id);
+
+describe('특정 요소 삭제', () => {
+  test('배열에서 삭제할 요소의 id를 인수로 전달하면 해당 요소를 삭제.', () => {
+    expect(removeTodo(todos, 2)).toEqual(
+      [
+        { id: 3, content: 'HTML', completed: false },
         { id: 1, content: 'Javascript', completed: false }
       ]
     );
